d382da5e91008b2119a949df362312df0b0a0255
Huyh Huyhu <2509roblox@gmail.com>
2023-03-30 10:26:20 +0700

    ciuofaef

--- main.js
+++ main.js
@@ -60,12 +60,12 @@ Array(200).fill().forEach(addStar);
 
 // Background
 
-const spaceTexture = new THREE.TextureLoader().load('https://tranlehuyhoang.github.io/Portfolio/space.jpg');
+const spaceTexture = new THREE.TextureLoader().load('./img/space.jpg');
 scene.background = spaceTexture;
 
 // Avatar
 
-const jeffTexture = new THREE.TextureLoader().load('https://tranlehuyhoang.github.io/Portfolio//hoang.png');
+const jeffTexture = new THREE.TextureLoader().load('./img/hoang.png');
 
 const jeff = new THREE.Mesh(new THREE.BoxGeometry(3, 3, 3), new THREE.MeshBasicMaterial({ map: jeffTexture }));
 
@@ -73,8 +73,8 @@ scene.add(jeff);
 
 // Moon
 
-const moonTexture = new THREE.TextureLoader().load('https://tranlehuyhoang.github.io/Portfolio//moon.jpg');
-const normalTexture = new THREE.TextureLoader().load('https://tranlehuyhoang.github.io/Portfolio//normal.jpg');
+const moonTexture = new THREE.TextureLoader().load('./img/moon.jpg');
+const normalTexture = new THREE.TextureLoader().load('./img/normal.jpg');
 
 const moon = new THREE.Mesh(
   new THREE.SphereGeometry(3, 32, 32),
